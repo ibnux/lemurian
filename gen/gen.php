93da63741f3fa29c57c28767590263605051b20e
iBNu Maksum <me@ibnux.net>
2022-07-27 23:31:00 +0700

    ganti ke bulma

---
+++ gen/gen.php
@@ -0,0 +1,8 @@
+<?php
+
+$json = json_decode(file_get_contents("lemurian.json"),true);
+$hasil = "";
+foreach($json as $k=>$v){
+    $hasil .= '<span class="tag is-link is-medium m-1" class="js-modal-trigger" data-target="modal-js-lemurian" onclick="showPopUp(\''.$k.'\',\''.$v.'\')">'.$k.'</span>';
+}
+file_put_contents("hasil.txt",$hasil);
