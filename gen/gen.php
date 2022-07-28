5b5e943682b9c50b29feac6eae391c0ab43706a6
Ibnu Maksum <ibnumaksum@gmail.com>
2022-07-28 10:21:21 +0700

    Generator nama

--- gen/gen.php
+++ gen/gen.php
@@ -3,6 +3,6 @@
 $json = json_decode(file_get_contents("lemurian.json"),true);
 $hasil = "";
 foreach($json as $k=>$v){
-    $hasil .= '<span class="tag is-link is-medium m-1" class="js-modal-trigger" data-target="modal-js-lemurian" onclick="showPopUp(\''.$k.'\',\''.$v.'\')">'.$k.'</span>';
+    $hasil .= '<span class="tag is-link is-medium m-1" class="js-modal-trigger" data-target="modal-js-lemurian" title="'.$v.'" onclick="showPopUp(\''.$k.'\',\''.$v.'\')">'.$k.'</span>';
 }
 file_put_contents("hasil.txt",$hasil);
