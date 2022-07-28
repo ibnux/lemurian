cba4c5dc5ee68a589f82a188ab80cdfed7d8506c
Ibnu Maksum <ibnumaksum@gmail.com>
2022-07-28 13:41:52 +0700

    urut berdasarkan nama

--- gen/gen.php
+++ gen/gen.php
@@ -1,6 +1,7 @@
 <?php
 
 $json = json_decode(file_get_contents("lemurian.json"),true);
+ksort($json);
 $hasil = "";
 foreach($json as $k=>$v){
     $hasil .= '<span class="tag is-link is-medium m-1" class="js-modal-trigger" data-target="modal-js-lemurian" title="'.$v.'" onclick="showPopUp(\''.$k.'\',\''.$v.'\')">'.$k.'</span>';
